8738f4a43c569a8d5dcb396e4810c0f785cde0d4
yuch7 <yuchen7990@gmail.com>
2016-01-22 23:58:29 -0500

    working? maybe

--- leap.cpp
+++ leap.cpp
@@ -21,14 +21,17 @@ void LeapEventListener::onConnect(const Controller& controller) {
 	std::cout << "Connected" << std::endl;
     // Enable gestures, set Config values:
     controller.enableGesture(Gesture::TYPE_SWIPE);
-    controller.enableGesture(Gesture::TYPE_INVALID);
+    controller.enableGesture(Gesture::TYPE_INVALID, false);
     controller.enableGesture(Gesture::TYPE_CIRCLE);
-    controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
+    // controller.enableGesture(Gesture::TYPE_SCREEN_TAP);
     controller.enableGesture(Gesture::TYPE_KEY_TAP);
-    
-    // controller.config().setFloat("Gesture.Swipe.MinLength", 200.0);
-    // controller.config().setFloat("Gesture.Circle.MinRadius", );
-    // controller.config().save();
+
+    controller.config().setFloat("Gesture.Circle.MinRadius", 100);
+    controller.config().setFloat("Gesture.Circle.MinArc", 30);
+    controller.config().setFloat("Gesture.Swipe.MinLength", 200);
+    // controller.config().setFloat("Gesture.KeyTap.Min")
+
+    controller.config().save();
 }
 
 
@@ -36,12 +39,45 @@ void LeapEventListener::onDisconnect(const Controller& controller) {
     std::cout << "Disconnected" << std::endl;
 }
 
-
-void LeapEventListener::onFrame(const Controller& controller) {
-	std::cout << "New frame available" << std::endl;
-    Leap::Frame frame = controller.frame();
+void handleSwipe(){
+	std::cout << "Swipe" << std::endl;
 }
 
+void handleTap(){
+	std::cout << "TAP" << std::endl;
+}
+
+void handleCircle(){
+	std::cout << "Circle" << std::endl;
+}
+
+
+void LeapEventListener::onFrame(const Controller& controller) {
+    Leap::Frame frame = controller.frame();
+
+    Leap::GestureList gestures = frame.gestures();
+	for(Leap::GestureList::const_iterator gl = gestures.begin(); gl != frame.gestures().end(); gl++){
+	    switch ((*gl).type()) {
+	        case Leap::Gesture::TYPE_CIRCLE:
+	            //Handle circle gestures
+	        	handleCircle();
+	            break;
+	        case Leap::Gesture::TYPE_KEY_TAP:
+	        	//Handle key tap gestures
+	        	handleTap();
+	            break;
+	        case Leap::Gesture::TYPE_SWIPE:
+	            //Handle swipe gestures
+	        	handleSwipe();
+	            break;
+	        default:
+	            //Handle unrecognized gestures
+	        	std::cout << "Invalid" << std::endl;
+	            break;
+    	}
+    }
+
+}
 
 int main(int argc, char ** argv) {
 	Controller controller;
